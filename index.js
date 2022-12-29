17daf5ba19570abe18a342ca74f4ec6b21b766b5
Duane Grell <duanegrell@gmail.com>
2022-12-29 15:10:06 -0500

    commit tree ff9222f0e1b7d4b06a2dec33d8b211a010a9da33

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction (name) {
+    return (`Hi, my name is ${name}.` )
+}
+
+introduction ("Duane")
+
+function introductionWithLanguage (name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+function introductionWithLanguageOptional (name, language) {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
+function introductionWithLanguageOptional (name = "User", language = "JavaScript") {
+    return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+
